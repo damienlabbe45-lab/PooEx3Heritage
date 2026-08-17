7fa1561f1efacdcd9426207e7397002299bd79af
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-17 15:19:57 +0200

    modification de la méthode toString

--- Person.java
+++ Person.java
@@ -3,6 +3,7 @@ public class Person {
     private String firstName;
     private int age;
     private String adress;
+    private City cityBirth;
 
     public Person(String name, String firstname, int age, String adress){
         this.lastName = name;
@@ -11,10 +12,18 @@ public class Person {
         this.adress = adress;
     }
 
+    public Person(String name, String firstname, int age, String adress, City city){
+        this.lastName = name;
+        this.firstName = firstname;
+        this.age = age;
+        this.adress = adress;
+        cityBirth = city;
+    }
+
     @Override
     public String toString() {
         return "Person [lastName=" + lastName + ", firstName=" + firstName + ", age=" + age + ", adress=" + adress
-                + "]";
+                + ", citybirth" + cityBirth.city() + "]";
     }
     
 }
