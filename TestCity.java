dddaa67de6423c5006528667c7d2357f9623ae04
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-17 13:13:49 +0200

    ajouts du test du 2ème constructeur

--- TestCity.java
+++ TestCity.java
@@ -1,14 +1,15 @@
 public class TestCity {
      public static void main(String[] args) {
         if( args.length > 0) throw new IllegalArgumentException(" pas d'arguments");
-        City toulouse = new City("France","Toulouse", 45000);
+        City toulouse = new City("Toulouse", 45000);
         City paris = new City("France","Paris", 8900000);
         City barcelone = new City("Espagne","Barcelone", 890000);
         System.out.println(toulouse.getnumberResidents());
-        System.out.println(paris.getnameCity());
+        System.out.println(paris.getnameCountry());
         System.out.println(barcelone.getnameCountry());
         //toulouse.numberResidents = 3566987;
-        System.out.println(toulouse.getnameCity());
+        toulouse.setNameCountryUser("France");
+        System.out.println(toulouse.getnameCountry());
      }
     
 }
