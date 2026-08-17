da12c7d88c63d9e17408b6114a1761ede9f2fb21
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-17 15:01:26 +0200

    ajout du constructeur Person

--- Person.java
+++ Person.java
@@ -1,3 +1,16 @@
 public class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
+    private String adress;
+
+    public Person(String name, String firstname, int age, String adress){
+        this.lastName = name;
+        this.firstName = firstname;
+        this.age = age;
+        this.adress = adress;
+    }
+
+    
     
 }
