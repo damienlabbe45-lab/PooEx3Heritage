17fb7d2089b25ca99e7272031b466da6881e57f0
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-17 11:20:43 +0200

    ajout du constructeur City

--- City.java
+++ City.java
@@ -3,6 +3,12 @@ public class City {
     public String nameCountry;
     public int numberResidents;
 
+    public City(String country, String city, int number){
+        this.nameCountry = country;
+        this.nameCity = city;
+        this.numberResidents = number;
+    }
+
     
     
 }
