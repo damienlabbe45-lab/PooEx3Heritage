11db9c073c1e249552c8916b80e8670076848d1f
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-18 11:37:07 +0200

    suppression de lignes inutiles dans main.java

--- src/fr/fms/entities/TestCapital.java
+++ src/fr/fms/entities/TestCapital.java
@@ -4,7 +4,6 @@ public class TestCapital {
 
     public static void main(String[] args) {
         if( args.length > 0) throw new IllegalArgumentException(" pas d'arguments");
-        System.out.println(City.counter);
         City paris = new Capital("France","Paris", 2000000, "Tour Eiffel");
         City londres = new Capital("GB","Londres", 940000, "BigBen");
         System.out.println(paris);
