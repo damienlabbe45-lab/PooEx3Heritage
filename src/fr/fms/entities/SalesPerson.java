a84c9f0dc2dab650de90ed4e52ab8a6072dc4735
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-18 14:49:40 +0200

    ajout des constructeurs et du toString

--- src/fr/fms/entities/SalesPerson.java
+++ src/fr/fms/entities/SalesPerson.java
@@ -1,5 +1,27 @@
 package fr.fms.entities;
 
 public class SalesPerson extends Person{
+
+    String entreprise;
+    double revenue;
+
+    public SalesPerson(String name, String firstname, int age, String adress, String entreprise, double revenue) {
+        super(name, firstname, age, adress);
+        this.entreprise = entreprise;
+        this.revenue = revenue;
+    }
+
+    public SalesPerson(String name, String firstname, int age, String adress, City city, String entreprise,
+            double revenue) {
+        super(name, firstname, age, adress, city);
+        this.entreprise = entreprise;
+        this.revenue = revenue;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Entreprise : " + entreprise + ", % CA :" + revenue;
+    }
+    
     
 }
