9f26c85a72d1b30d1ecdb3d1fd8c50f9f03cb06b
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-18 11:07:39 +0200

    ajout des constructeurs

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -2,5 +2,22 @@ package fr.fms.entities;
 
 public class Capital extends City{
     String landmark;
+
+    public Capital(String country, String city, int number, String landmark) {
+        super(country, city, number);
+        this.landmark = landmark;
+    }
+
+    public Capital(String city, int number, String landmark) {
+        super(city, number);
+        this.landmark = landmark;
+    }
+
+    public Capital(String city, String country, String landmark) {
+        super(city, country);
+        this.landmark = landmark;
+    }
+
+    
     
 }
