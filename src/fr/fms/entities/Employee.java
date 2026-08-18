179272fae3ef840e13167c7740f3319bd883a469
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-18 12:00:35 +0200

    ajout des constructeurs et du toString

--- src/fr/fms/entities/Employee.java
+++ src/fr/fms/entities/Employee.java
@@ -1,5 +1,29 @@
 package fr.fms.entities;
 
 public class Employee extends Person{
+    String entreprise;
+    double salaries;
+    
+    public Employee(String name, String firstname, int age, String adress, String entreprise, double salaries) {
+        super(name, firstname, age, adress);
+        this.entreprise = entreprise;
+        this.salaries = salaries;
+    }
+    
+    public Employee(String name, String firstname, int age, String adress, City city, String entreprise,
+            double salaries) {
+        super(name, firstname, age, adress, city);
+        this.entreprise = entreprise;
+        this.salaries = salaries;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [lastName=" + lastName + ", entreprise=" + entreprise + ", salaries=" + salaries
+                + ", firstName=" + firstName + ", age=" + age + ", adress=" + adress + ", cityBirth=" + cityBirth + "]";
+    }
+
+    
+
     
 }
