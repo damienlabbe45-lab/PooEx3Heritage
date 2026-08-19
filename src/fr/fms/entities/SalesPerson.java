7503caea3dc94b0d4d007ef0f3e5c70cfd2cc665
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-19 10:01:37 +0200

    modifs de setRevenue pour faire changer les conditions des ifs

--- src/fr/fms/entities/SalesPerson.java
+++ src/fr/fms/entities/SalesPerson.java
@@ -26,7 +26,7 @@ class SalesPerson extends Person{
     }
 
     public void setRevenue(double revenue) {
-        if(revenue < REVENUMAX && REVENUMIN > revenue)this.revenue = revenue;
+        if(revenue < REVENUMAX && revenue > REVENUMIN)this.revenue = revenue;
         else if(REVENUMAX > revenue) System.out.println("Le revenu n'est pas assez élevé");
         else System.out.println("Le revenu est trop élevé, l'entreprise va faire faillite");
     }
