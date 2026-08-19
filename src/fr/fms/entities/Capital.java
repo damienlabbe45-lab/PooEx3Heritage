21bf9bfb6254a81197fb4a85c36ec11896219b4e
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-19 13:11:37 +0200

    création de la javadoc

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -1,39 +1,85 @@
 package fr.fms.entities;
 
-public class Capital extends City{
+/**
+ * Représente une capitale, qui est une extension de {@link City} associée à un monument représentatif[span_43](start_span)[span_43](end_span).
+ */
+public class Capital extends City {
+
+    /** Le monument emblématique de la capitale[span_44](start_span)[span_44](end_span). */
     String landmark;
 
+    /**
+     * Constructeur avec pays, ville, nombre d'habitants et monument[span_45](start_span)[span_45](end_span).
+     *
+     * @param country Nom du pays[span_46](start_span)[span_46](end_span)
+     * @param city Nom de la ville[span_47](start_span)[span_47](end_span)
+     * @param number Nombre d'habitants[span_48](start_span)[span_48](end_span)
+     * @param landmark Monument représentatif[span_49](start_span)[span_49](end_span)
+     */
     public Capital(String country, String city, int number, String landmark) {
         super(country, city, number);
         this.landmark = landmark;
     }
 
+    /**
+     * Constructeur avec ville, nombre d'habitants et monument[span_50](start_span)[span_50](end_span).
+     *
+     * @param city Nom de la ville[span_51](start_span)[span_51](end_span)
+     * @param number Nombre d'habitants[span_52](start_span)[span_52](end_span)
+     * @param landmark Monument représentatif[span_53](start_span)[span_53](end_span)
+     */
     public Capital(String city, int number, String landmark) {
         super(city, number);
         this.landmark = landmark;
     }
 
+    /**
+     * Constructeur avec ville, pays et monument[span_54](start_span)[span_54](end_span).
+     *
+     * @param city Nom de la ville[span_55](start_span)[span_55](end_span)
+     * @param country Nom du pays[span_56](start_span)[span_56](end_span)
+     * @param landmark Monument représentatif[span_57](start_span)[span_57](end_span)
+     */
     public Capital(String city, String country, String landmark) {
         super(city, country);
         this.landmark = landmark;
     }
 
+    /**
+     * Définit le monument emblématique[span_58](start_span)[span_58](end_span).
+     *
+     * @param landmark Le nom du monument[span_59](start_span)[span_59](end_span)
+     */
     public void setLandmark(String landmark) {
         this.landmark = landmark;
     }
 
+    /**
+     * Obtient le monument emblématique[span_60](start_span)[span_60](end_span).
+     *
+     * @return Le nom du monument[span_61](start_span)[span_61](end_span)
+     */
     public String getLandmark() {
         return landmark;
     }
 
+    /**
+     * Retourne la description de la capitale en ajoutant le monument au texte hérité de {@link City}[span_62](start_span)[span_62](end_span).
+     *
+     * @return Une chaîne décrivant la capitale et son monument[span_63](start_span)[span_63](end_span)
+     */
     @Override
     public String toString() {
-        return super.toString() + ", monument: " + landmark ;
+        return super.toString() + ", monument: " + landmark;
     }
 
+    /**
+     * Retourne le nom de la ville et du pays en y ajoutant le monument[span_64](start_span)[span_64](end_span).
+     *
+     * @return Une chaîne décrivant la localisation et le monument[span_65](start_span)[span_65](end_span)
+     */
     @Override
     public String residents() {
-        return super.residents() + ", monument: " + landmark ;
+        return super.residents() + ", monument: " + landmark;
     }
-    
 }
--- src/fr/fms/entities/City.java
+++ src/fr/fms/entities/City.java
@@ -1,12 +1,30 @@
 package fr.fms.entities;
 
-
+/**
+ * Représente une ville avec son nom, son pays et son nombre d'habitants.
+ * Maintient également un compteur global du nombre d'instances de villes créées.
+ */
 public class City {
+
+    /** Nom de la ville. */
     protected String nameCity;
+
+    /** Nom du pays. */
     protected String nameCountry;
+
+    /** Nombre d'habitants de la ville. */
     protected int numberResidents;
+
+    /** Compteur global d'instances créées de la classe City. */
     public static int counter = 0;
 
+    /**
+     * Constructeur complet initialisant le pays, la ville et le nombre d'habitants.
+     *
+     * @param country Nom du pays
+     * @param city Nom de la ville
+     * @param number Nombre d'habitants
+     */
     public City(String country, String city, int number){
         setNameCountry(country);
         setNameCity(city);
@@ -14,6 +32,13 @@ public class City {
         counter++;
     }
 
+    /**
+     * Constructeur initialisant le nom de la ville et le nombre d'habitants.
+     * Le pays est défini par défaut à "unknow".
+     *
+     * @param city Nom de la ville
+     * @param number Nombre d'habitants
+     */
     public City(String city, int number){
         setNameCity(city);
         setNumberResidents(number);
@@ -21,6 +46,13 @@ public class City {
         counter++;
     }
 
+    /**
+     * Constructeur initialisant le nom de la ville et le nom du pays.
+     * Le nombre d'habitants est initialisé à 0.
+     *
+     * @param city Nom de la ville
+     * @param country Nom du pays
+     */
     public City(String city, String country){
         setNameCity(city);
         setNameCountry(country);
@@ -28,54 +60,112 @@ public class City {
         counter++;
     }
 
+    /**
+     * Met à jour le nom du pays si la valeur actuelle est "unknow".
+     *
+     * @param country Le nouveau nom du pays à attribuer
+     */
     protected void setNameCountryUser(String country){
         if(this.nameCountry.equals("unknow")) setNameCountry(country);
     }
 
+    /**
+     * Définit le nom du pays.
+     *
+     * @param country Le nom du pays
+     */
     protected void setNameCountry(String country){
         nameCountry = country;
     }
 
+    /**
+     * Définit le nom de la ville.
+     *
+     * @param city Le nom de la ville
+     */
     protected void setNameCity(String city){
         nameCity = city;
     }
 
+    /**
+     * Définit le nombre d'habitants de la ville.
+     * Affiche un message d'avertissement si la population est inférieure ou égale à 20 000.
+     *
+     * @param number Le nombre d'habitants
+     */
     protected void setNumberResidents(int number){
         if(number > 20000) numberResidents = number;
         else System.out.println("Une ville a au moins 20 000 habitants");
     }
 
+    /**
+     * Obtient le nom du pays.
+     *
+     * @return Le nom du pays
+     */
     public String getNameCountry(){
         return nameCountry;
     }
 
+    /**
+     * Obtient le nom de la ville.
+     *
+     * @return Le nom de la ville
+     */
     public String getNameCity(){
         return nameCity;
     }
 
+    /**
+     * Obtient le nombre d'habitants.
+     *
+     * @return Le nombre d'habitants
+     */
     public int getNumberResidents(){
         return numberResidents;
     }
 
+    /**
+     * Génère une représentation textuelle de la ville sous forme de bloc de données.
+     *
+     * @return Une chaîne décrivant l'objet City
+     */
     public String city() {
         if(numberResidents != 0) return "City [nameCity=" + nameCity + ", nameCountry=" + nameCountry + ", numberResidents=" + numberResidents
                 + "]";
         else return "City [nameCity=" + nameCity + ", nameCountry=" + nameCountry + "]";
     }
 
+    /**
+     * Retourne une description lisible de la ville.
+     *
+     * @return Une chaîne indiquant la ville, le pays et le nombre d'habitants
+     */
+    @Override
     public String toString(){
         return "ville de " + nameCity + " en " + nameCountry + " ayant " + numberResidents + " habitants.";
     }
 
+    /**
+     * Affiche une valeur de test fixe dans la console.
+     */
     public void display(){
         System.out.println("Ville de Toulouse en France ayant 45000");
     }
 
+    /**
+     * Affiche les informations de l'instance courante dans la console.
+     */
     public void print(){
-        System.out.println("La ville de " + nameCity + " en " + nameCountry + " ayant "  + numberResidents + " habitants");
+        System.out.println("La ville de " + nameCity + " en " + nameCountry + " ayant " + numberResidents + " habitants");
     }
-    
+
+    /**
+     * Retourne une chaîne contenant le nom de la ville et du pays.
+     *
+     * @return Une chaîne au format "NomVille, NomPays"
+     */
     public String residents(){
-        return nameCity + ", " + nameCountry ;
+        return nameCity + ", " + nameCountry;
     }
 }
